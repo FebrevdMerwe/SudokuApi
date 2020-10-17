576195d53e7fb912bf0fe82ec85e4e1cba4ab2fb
Febre <febrevdmerwe@gmail.com>
2020-10-17 23:17:43 +0200

    Added Sudoku api

---
+++ SudokuApi/Controllers/SudokuController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Sudoku.Generator;
+using Sudoku.Puzzle;
+using Sudoku.Solver;
+using Sudoku.Validator;
+using SudokuApi.Models;
+
+namespace SudokuApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SudokuController : ControllerBase
+    {
+        [HttpGet()]
+        [Route("Generate/{difficulty}")]
+        public ActionResult<SudokuPuzzleDTO> Generate(SudokuDifficulty difficulty, int? seed)
+        {
+            ISudokuGenerator generator = new SudokuGenerator();
+            ISudokuPuzzle puzzle = generator.Generate(difficulty, seed);
+
+            SudokuPuzzleDTO puzzleDTO = new SudokuPuzzleDTO()
+            {
+                Puzzle = puzzle.ToString(),
+                Seed = puzzle.Id
+            };
+
+            return puzzleDTO;
+        }
+
+        [HttpPost()]
+        [Route("Solve")]
+        public ActionResult<SudokuPuzzleDTO> Solve(SudokuPuzzleDTO puzzle)
+        {
+            ISudokuValidator validator = new SudokuValidator();
+            ISudokuSolver solver = new SudokuSolver(puzzle.Puzzle, validator);
+
+            ISudokuPuzzle solved = solver.Solve();
+
+            SudokuPuzzleDTO puzzleDTO = new SudokuPuzzleDTO()
+            {
+                Puzzle = solved.ToString(),
+                Seed = puzzle.Seed
+            };
+
+            return puzzleDTO;
+        }
+    }
+}
---
+++ SudokuApi/Models/SudokuPuzzleDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SudokuApi.Models
+{
+    public class SudokuPuzzleDTO
+    {
+        public string Puzzle { get; set; }
+        public int Seed { get; set; }
+    }
+}
